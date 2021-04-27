7932bb40a3ca7311c708e532b84a74392f4c26e0
gautier kiss <gkgkgk395@gmail.com>
2021-04-27 22:17:03 +0200

    done

--- srcs/error/error1.c
+++ srcs/error/error1.c
@@ -1,37 +1,85 @@
 #include "./../include/ft_ssl.h"
 
-static void print_error(char **argv)
+static void print_nb_arg_error() {
+    printf("No arguments passed to the program\n");
+    exit(EXIT_FAILURE);
+}
+
+static void print_usage_error(char **argv)
 {
     printf("ft_ssl: Error: %s is an invalid command\n", argv[1]);
     printf("Standard commands:\n");
     printf("Message Digest commands:\n");
     printf("Message Digest commands:\n");
-    printf("- md5");
-    printf("- sha256\n");
-    printf("Cipher commands:");
+    printf("\t- md5\n");
+    printf("\t- sha256\n");
+    printf("Cipher commands:\n");
     exit(EXIT_FAILURE);
 }
 
-static void check_type_args(char **argv)
+static void print_option_error(char **argv, char opt) {
+    printf("%s: illegal option -- %c\n", argv[1], opt);
+    printf("usage: md5 [-pqrs] [-s string] [files ...]\n");
+    exit(EXIT_FAILURE);
+}
+
+static void print_string_error(char **argv) {
+    printf("%s: option requires an argument -- s\n", argv[1]);
+    printf("usage: md5 [-pqrs] [-s string] [files ...]\n");
+    exit(EXIT_FAILURE);
+}
+
+static bool isOptionString(uint8_t *arg) {
+    return (strchr((char*)arg, 's') != NULL);
+}
+
+static void isUnknownOption(char **argv, uint8_t *arg) {
+    int i = 1;
+
+    while(arg[i]) {
+        if (arg[i] != 'q' && arg[i] != 'r' && arg[i] != 'p' && arg[i] != 's')
+            print_option_error(argv, arg[i]);
+        ++i;
+    }
+}
+
+static bool isOption(uint8_t *arg) {
+    return (arg[0] == '-');
+}
+
+static void check_args(char **argv) {
+    int i = 2;
+    while(argv[i]) {
+        if(isOption((uint8_t*)argv[i])) {
+            isUnknownOption(argv, (uint8_t*)argv[i]);
+            if (isOptionString((uint8_t*)argv[i])) {
+                if (argv[i + 1] == NULL){
+                    print_string_error(argv);
+                }
+            }
+        }
+        ++i;
+    }
+}
+
+static void check_cmd(char **argv)
 {
     if(strcmp(argv[1], "md5") == 0)
         return ;
     if(strcmp(argv[1], "sha256") == 0)
         return ;
-    print_error(argv);
+    print_usage_error(argv);
 }
 
 static void check_nb_args(int argc)
 {
     if (argc == 1)
-    {
-        printf("No arguments passed to the program\n");
-        exit(EXIT_FAILURE);
-    }
+        print_nb_arg_error();
 }
 
 void     check_error(int argc, char **argv)
 {
     check_nb_args(argc);
-    check_type_args(argv);
+    check_cmd(argv);
+    check_args(argv);
 }
--- srcs/sha256/sha256.c
+++ srcs/sha256/sha256.c
@@ -1,21 +1,68 @@
 #include "./../include/ft_ssl.h"
 
-static void output(t_data *data, uint8_t *hashed, t_node *node) {
-    data->opts->p = 0;
-    node->type = 0;
-    for (uint32_t i = 0; i < 32; ++i) {
+static void output_error(t_data *data, t_node *node) {
+    printf("%s: %s: No such file or directory\n", data->cmd, node->file_name);
+}
+
+static void output_hash(uint8_t *hashed, uint32_t size) {
+    for (uint32_t i = 0; i < size; ++i) {
         printf("%02x", hashed[i]);
+    }
+}
+
+static void output_normal(t_data *data, uint8_t *hashed, t_node *node) {
+    if (data->opts->q == 1) {
+        output_hash(hashed, 32);    
+    } else {
+        if (data->opts->r == 0) {
+            if(node->type == STRING) {
+                printf("%s (\"%s\") = ", data->uppercase_cmd, node->arg);
+            } else if (node->type == FILE) {
+                printf("%s (%s) = ", data->uppercase_cmd, node->file_name);
+            }
+            output_hash(hashed, 32);
+            
+        } else {
+            output_hash(hashed, 32);
+            if(node->type == STRING) {
+                printf(" \"%s\"", node->arg);
+            } else if (node->type == FILE) {
+                printf(" %s", node->file_name);
+            }
+        }
     }
     printf("\n");
 }
 
+static void output(t_data *data, uint8_t *hashed, t_node *node) {
+    if(node->arg == NULL) {
+        output_error(data, node);
+    } else {
+        output_normal(data, hashed, node);
+    }
+}
+
+static void output_input(t_data *data) {
+    uint8_t     hashed[32];
+    if(data->input) {
+        if (data->opts->p) {
+            printf("%s", data->input);
+        }
+        hash_sha256(data->input, hashed);
+        output_hash(hashed, 32);
+        printf("\n");
+    }
+}
+
 void        ft_sha256(t_data *data)
 {
-    uint8_t     hashed[16];
+    uint8_t     hashed[32];
 
     t_node *tmp = data->node;
+    output_input(data);
     while(tmp) {
-        hash_sha256(tmp->arg, hashed);
+        if(tmp->arg)
+            hash_sha256(tmp->arg, hashed);
         output(data, hashed, tmp);
         tmp = tmp->next;
     }
